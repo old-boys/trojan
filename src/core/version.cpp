85f19979104da646a311f446489f7f85f031d744
456 <freereaper.svip@gmail.com>
2020-04-14 23:32:43 +0800

    bump v1.19.4.
    Signed-off-by: 456 <freereaper.svip@gmail.com>

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.19.3");
+const string Version::version("1.19.4");
 
 string Version::get_version() {
     return version;
